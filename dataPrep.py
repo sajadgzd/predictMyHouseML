c5fc01de9fc0037833d1671b5cdcfb9510205360
sajad1993gh <sajad1993gh@gmail.com>
2020-11-22 01:19:44 -0500

    finished binary

--- dataPrep.py
+++ dataPrep.py
@@ -12,10 +12,11 @@ with open('nyc_housing.csv', mode='w') as employee_file:
 
     # cleaning data from urls and descriptions
     for row in inputfile:
-        if row[2] == "new york city" and (float(row[6]) <= 200 or float(row[4]) <= 400):
+        # and (float(row[6]) <= 200 or float(row[4]) <= 400)
+        if row[2] == "new york city":
                 print("Skipping an out of range house with a price of ", row[4], " and SQFT of ", row[6])
-        
-        if row[2] == "new york city" and float(row[4]) > 400 and float(row[6]) > 200:
+        # and float(row[4]) > 400 and float(row[6]) > 200
+        if row[2] == "new york city":
             # print(row)
             nyc_housing.writerow([row[0], row[2], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[19], row[20], row[21]])
             count_nyc +=1
