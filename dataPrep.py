e60929d54a6ad4c60cf9d40f8708ef1af30ad0d0
sajad1993gh <sajad1993gh@gmail.com>
2020-11-21 19:24:52 -0500

    updated cvs

--- dataPrep.py
+++ dataPrep.py
@@ -3,7 +3,7 @@ import csv
 
 inputfile = csv.reader(open('housing.csv','r'))
 count_nyc = 0
-#  15 listings less than 500 price
+#  40 listings less than 400 price and 200 sqft
 
 with open('nyc_housing.csv', mode='w') as employee_file:
     print("processing...\n")
@@ -12,9 +12,10 @@ with open('nyc_housing.csv', mode='w') as employee_file:
 
     # cleaning data from urls and descriptions
     for row in inputfile:
-        if row[2] == "new york city" and float(row[4]) <= 400:
-            print("Skipping an out of range house with a very low price of ", row[4])
-        if row[2] == "new york city" and float(row[4]) > 400:
+        if row[2] == "new york city" and (float(row[6]) <= 200 or float(row[4]) <= 400):
+                print("Skipping an out of range house with a price of ", row[4], " and SQFT of ", row[6])
+        
+        if row[2] == "new york city" and float(row[4]) > 400 and float(row[6]) > 200:
             # print(row)
             nyc_housing.writerow([row[0], row[2], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[19], row[20], row[21]])
             count_nyc +=1
